bc2a647b80cca2fd36d5014e317d98673f7b9b3b
kasur.rusak404@gmail.com <1631Nz94@>
2021-09-30 20:01:04 +0700

    buat design awal buttom menu dan login

---
+++ app/src/main/java/muji/dev/tokopedih/NavigationActivity.kt
@@ -0,0 +1,57 @@
+package muji.dev.tokopedih
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import muji.dev.tokopedih.databinding.ActivityNavigationBinding
+import muji.dev.tokopedih.ui.login.LoginActivity
+import muji.dev.tokopedih.util.Prefs
+
+class NavigationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNavigationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityNavigationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_navigation)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_keranjang, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+        navView.setOnItemSelectedListener {
+
+            if (it.itemId == R.id.navigation_notifications) {
+                val s = Prefs(this)
+                if (s.getIsLogin()) {
+                    Log.d("TAG", "sudah login")
+                    navController.navigate(it.itemId)
+                } else {
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    Log.d("TAG", "Belum login, pindah ke halaman login")
+                }
+            } else {
+                navController.navigate(it.itemId)
+                Log.d("TAG", "onCreate: yang lain" + it.itemId)
+            }
+
+            return@setOnItemSelectedListener true
+        }
+    }
+}
---
+++ app/src/main/java/muji/dev/tokopedih/ui/keranjang/KeranjangFragment.kt
@@ -0,0 +1,41 @@
+package muji.dev.tokopedih.ui.keranjang
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import muji.dev.tokopedih.databinding.FragmentKeranjangBinding
+
+
+class KeranjangFragment : Fragment() {
+
+    private lateinit var notificationsViewModel: KeranjangViewModel
+    private var keranjangBinding: FragmentKeranjangBinding? = null
+
+    private val binding get() = keranjangBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        notificationsViewModel = ViewModelProvider(this).get(KeranjangViewModel::class.java)
+        keranjangBinding = FragmentKeranjangBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        keranjangBinding = null
+    }
+
+}
